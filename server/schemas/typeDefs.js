298d4c3797566c812b4b972c4138c20851c25e45
Tevissaur <tevisreilly1@gmail.com>
2021-12-19 09:39:22 -0600

    server is apollo server, working on resolvers and typedefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,23 @@
+const { gql } = require('apollo-server-express')
+
+const typeDefs = gql`
+    type Book {
+        _id: ID!
+        title: String!
+    }
+
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        password: String!
+        savedBooks: [Book]
+    }
+
+    type Query {
+        books: [Book]!
+        users: [User]!
+    }
+`
+
+module.exports = typeDefs
