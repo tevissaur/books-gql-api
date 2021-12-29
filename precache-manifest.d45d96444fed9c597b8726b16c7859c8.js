90264ef7b37f9bdd19e3001ef519dc3367b83a4b
Tevissaur <berserkboar@protonmail.com>
2021-12-29 12:19:54 -0600

    Updates

---
+++ precache-manifest.d45d96444fed9c597b8726b16c7859c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd593c4fa0a3008dbfcf3bfaa069efa4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ae445af6583564c7a01",
+    "url": "/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "04f3aad74d29d2eb1546",
+    "url": "/static/css/main.10507661.chunk.css"
+  },
+  {
+    "revision": "6ae445af6583564c7a01",
+    "url": "/static/js/2.7d7eaa1a.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/static/js/2.7d7eaa1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04f3aad74d29d2eb1546",
+    "url": "/static/js/main.64d6392b.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
